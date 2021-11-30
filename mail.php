a055ddca36666f6c8e40bc888b7fb3f1dc306b11
TrexCruz <63073316+TrexCruz@users.noreply.github.com>
2021-11-30 04:09:02 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,37 +1,35 @@
 
 <?php
-if(isset( $_POST['name']))
 $name = $_POST['name'];
-if(isset( $_POST['email']))
 $email = $_POST['email'];
-if(isset( $_POST['message']))
 $message = $_POST['message'];
-if(isset( $_POST['subject']))
 $subject = $_POST['subject'];
+header('Content-Type: application/json');
 if ($name === ''){
-echo "Name cannot be empty.";
-die();
+print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+exit();
 }
 if ($email === ''){
-echo "Email cannot be empty.";
-die();
+print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+exit();
 } else {
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo "Email format invalid.";
-die();
+print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+exit();
 }
 }
 if ($subject === ''){
-echo "Subject cannot be empty.";
-die();
+print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
+exit();
 }
 if ($message === ''){
-echo "Message cannot be empty.";
-die();
+print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
+exit();
 }
 $content="From: $name \nEmail: $email \nMessage: $message";
 $recipient = "eliscodavid@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
+print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
+exit();
 ?>
